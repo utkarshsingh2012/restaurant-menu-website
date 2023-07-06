274817b015a3d533650879ca3e3d0d534ed077a8
Utkarsh Prakash Singh <113572394+utkarshsingh2012@users.noreply.github.com>
2023-07-06 07:47:45 +0530

    Add files via upload

---
+++ restaurant-app/src/component/basics/menuApi.js
@@ -0,0 +1,97 @@
+const Menu = [
+  {
+  id: 1,
+  image: "images/maggi.jpg",
+  name:"maggi",
+  category: "breakfast",
+  price: "₹25",
+  description: "Savour a bowl of hot lip-smacking Maggi and some more fusion Maggi dishes at Hungry Head! You will be spoilt for choice with their different varieties of Maggi dishes!",
+  },
+  
+  
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoida",
+    category: "snacks",
+    price: "₹50",
+    description:
+      "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks.",
+  },
+
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "₹40",
+    description:
+      "Spicy Crispy Corn - healthy tea time snack recipe that can be prepared within few minutes.Its a crunchy, tasty vegetarian snack. Its loaded with the Indian-Chinese fusion flavors these crispy corns are our family favorite party snack.",
+  },
+
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "₹150",
+    description:
+      "Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala and bhatura/puri, a deep-fried bread made from maida.",
+  },
+
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "snacks",
+    price: "₹200",
+    description:
+      "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven.",
+  },
+
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "₹300",
+    description:
+      "A typical non-veg meal contains meat or fish along with other vegetarian dishes - for example, dal or sabzi (vegetable dish). My non-vegetarian thali/meals in this collection include not just delicious meat dishes, but also some mouthwatering vegetarian dishes.",
+  },
+
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "₹60",
+    description:
+      "Rasgulla Dodha Burfee Kaju Katli Flavourful Fusions - Milk (500 gms) Flavourful Fusions - Kaju (500 gms) Badam Halwa (200 gms)",
+  },
+
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "₹60",
+    description:
+      "This Rajma recipe is a lightly spiced, creamy and delicious Punjabi style curry made with protein rich kidney beans, aromatics like onions, ginger, garlic, fragrant spices and tangy tomatoes. ",
+  },
+
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "snacks",
+    price: "₹20",
+    description:
+      "A samosa is a fried South Asian pastry with a savoury filling, including ingredients such as spiced potatoes, onions, peas, meat or fish. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
+  },
+
+  
+  
+
+];
+
+export default Menu;
